d8bff125073d91479cc93021cc7d714e6df078f4
Dmitrii Mikhailov <31654782+Tariod@users.noreply.github.com>
2018-12-25 11:21:55 +0200

    Revert "feat: implement single machine algorithm"

--- sloth.js
+++ sloth.js
@@ -1,12 +1,9 @@
 'use strict';
 
 const submodules = [
-  'retardMinim',
   'schedule',
   'task',
   'taskList',
-  'tasksCompability',
-  'weightedRetardMinim',
 ].map(path => require('./lib/' + path));
 
 const tools = require('./lib/tools');
